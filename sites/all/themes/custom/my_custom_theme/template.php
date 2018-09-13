b74030847530246d1b8431b77c5b8ebbdec2a19a
Rohit Tiwari <rtiwari@evolenthealth.com>
2018-09-13 17:18:26 +0100

    Added code for contact form

--- sites/all/themes/custom/my_custom_theme/template.php
+++ sites/all/themes/custom/my_custom_theme/template.php
@@ -2,30 +2,27 @@
 
 
 
+/**
+ *  Implements hook_preprocess_node().
+ *  @todo  needs works
+ */
 
+function my_custom_theme_preprocess_node(&$variables) {
 
-function my_custom_theme_preprocess_entity(&$variables) {
-  switch ($variables['entity_type']) {
-    case 'paragraphs_item':
-      if (array_key_exists(PARAGRAPH_ITEM_IMAGE_FIELD_NAME, $variables['content'])) {
+  if (array_key_exists(NODE_IMAGE_FIELD_NAME, $variables['content'])) {
 
-        if ( array_key_exists(PARAGRAPH_ITEM_IMAGE_STYLE, $variables['content'])) {
-          $image_style = isset($variables['content'][PARAGRAPH_ITEM_IMAGE_STYLE]['#items'][0]['value']) ?
-          $variables['content'][PARAGRAPH_ITEM_IMAGE_STYLE]['#items'][0]['value'] : '';
-        }
-
-        if (!empty($image_style)) {
-          $variables['content'][PARAGRAPH_ITEM_IMAGE_FIELD_NAME][0]['#image_style'] = $image_style;
-        } else {
-          unset($variables['content'][PARAGRAPH_ITEM_IMAGE_FIELD_NAME]);
-        }
+      if ( array_key_exists(NODE_IMAGE_STYLE_FIELD, $variables['content'])) {
+        $image_style = isset($variables['content'][NODE_IMAGE_STYLE_FIELD]['#items'][0]['value']) ?
+        $variables['content'][NODE_IMAGE_STYLE_FIELD]['#items'][0]['value'] : '';
+      }  else if ($variables['node']->bundle == NODE_SUBTYPE) {
+        $image_style = 'thumbnail';
       }
-      break;
-
-    default:
-      # code...
-      break;
-  }
+      if (!empty($image_style) && $image_style != 'no_image') {
+        $variables['content'][NODE_IMAGE_FIELD_NAME][0]['#image_style'] = $image_style;
+      } else if ($image_style == 'no_image') {
+        unset($variables['content'][NODE_IMAGE_FIELD_NAME]);
+      }
+    }
 }
 
 
